b8ea9caaad1c8f375364b4c16f96322da2aa7712
camila <Bonillajanelka@gmail.com>
2022-08-24 19:45:28 -0400

    Encriptador terminado para pantalla 940px

---
+++ encriptadorf.js
@@ -0,0 +1,31 @@
+
+
+function encriptar(){
+    var texto = document.getElementById("inputTexto").value;
+    var txtCifrado = texto.replace(/e/g, "enter").replace(/o/g,"ober")
+    .replace(/i/g,"imes").replace(/a/g,"ai").replace(/u/g,"ufat");
+
+    limpiador(txtCifrado)
+     
+}
+
+function deseencriptar(){
+    var texto = document.getElementById("inputTexto").value;
+
+    var txtCifrado = texto.replace(/enter/g,"e").replace(/ober/g,"o")
+    .replace(/imes/g,"i").replace(/ai/g,"a").replace(/ufat/g,"u");
+
+    limpiador(txtCifrado)
+
+}
+function copy(){
+    var contenido = document.querySelector("#texto2").value;
+    navigator.clipboard.writeText(contenido)
+}
+function limpiador(txtCifrado){
+    document.getElementById("imgderecha").style.display ="none";
+    document.getElementById("texto1").style.display ="none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copy").style.display = "show"
+    document.getElementById("copy").style.display = "inherit"
+}
